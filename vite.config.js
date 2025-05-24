26a7caa4d5c0498018266f5b41132fb577b999ff
petitbonney <petitbonney@gmail.com>
2025-05-23 23:50:01 -0400

    Add tailwind and daisyui

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mc-mod-version/',
-  plugins: [react()],
-})
+  base: "/mc-mod-version/",
+  plugins: [react(), tailwindcss()],
+});
