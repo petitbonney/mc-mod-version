b74817223fd0cc921b2adcc85a2dd1b5b1b19517
petitbonney <petitbonney@gmail.com>
2025-05-24 01:11:59 -0400

    Add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 import { useState } from "react";
 import "./App.css";
 
-export const App = () => {
-  const [count, setCount] = useState(0);
+const MODRINTH_SEARCH_URL = "https://api.modrinth.com/v2/search?";
+const MODRINTH_VERSION_URL =
+  "https://api.modrinth.com/v2/project/{id|slug}/version";
+
+const App = () => {
+  const [results, setResults] = useState([]);
+
+  const updateSearch = (keywords) =>
+    fetch(
+      MODRINTH_SEARCH_URL +
+        new URLSearchParams({
+          query: keywords,
+          limit: 20,
+        })
+    )
+      .then((r) => r.json())
+      .then((_) => _.hits)
+      .then(setResults);
 
   return (
     <>
-      <button
-        className="btn btn-primary"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        count is {count}
-      </button>
+      <input
+        type="text"
+        className="input focus:outline-none"
+        onKeyDown={(evt) =>
+          evt.key == "Enter" && updateSearch(evt.currentTarget.value)
+        }
+      />
+      <div>
+        {results.map((mod, i) => (
+          <div key={i} className="flex">
+            <div
+              style={{ backgroundImage: `url(${mod.icon_url})` }}
+              className="min-h-10 min-w-10 bg-cover bg-no-repeat bg-center"
+            ></div>
+            {mod.title} {mod.description}
+          </div>
+        ))}
+      </div>
     </>
   );
 };
+
+export default App;
